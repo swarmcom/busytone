b9ff3bfe61a951b525f9f8b948a3a184c1a008e1
Roman Galeev <roman.galeev@ezuce.com>
2017-07-20 13:02:47 +0200

    Sync on call, not on ws events

--- src/tests/t_agent_noanswer.erl
+++ src/tests/t_agent_noanswer.erl
@@ -2,13 +2,14 @@
 -export([main/0]).
 
 main() ->
-	Agent = admin:new_agent(),
+	Agent = admin:new_agent( #{ skills => #{ english => true }, ring_timeout => 3 }),
 	agent:wait_ws(Agent, #{ <<"username">> => Agent }),
 	agent:available(Agent),
 	agent:wait_ws(Agent, #{ <<"command">> => <<"arelease">>, <<"releaseData">> => false }),
 	{ok, InQueueCall} = call_sup:originate(<<"9999">>),
 	agent:wait_ws(Agent, #{ <<"command">> => <<"setchannel">>, <<"state">> => <<"ringing">>, <<"channelid">> => <<"ch1">> }),
-	agent:wait_ws(Agent, #{ <<"command">> => <<"endchannel">>, <<"channelid">> => <<"ch1">> }, 15000),
+	[UUID1] = agent:wait_for_call(Agent),
+	call:wait_hangup(UUID1),
 	[UUID] = agent:wait_for_call(Agent),
 	ok = call:answer(UUID),
 	agent:wait_ws(Agent, #{ <<"command">> => <<"setchannel">>, <<"state">> => <<"oncall">>, <<"channelid">> => <<"ch2">> }),
