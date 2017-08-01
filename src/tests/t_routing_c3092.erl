0aaedc94343610d034ddc315461f24961b4ef737
Roman Galeev <roman.galeev@ezuce.com>
2017-08-01 11:34:28 +0200

    Wait for vqueue to set-up before doing tests

--- src/tests/t_routing_c3092.erl
+++ src/tests/t_routing_c3092.erl
@@ -3,23 +3,28 @@
 
 % C3092: Routing to the agent who is available for longest time
 
-receive_call(Queue, Agent) ->
+receive_call(Queue, Agent, Ch) ->
 	{ok, InCall} = call_sup:originate(Queue),
-	UUID = test_lib:answer(Agent),
+	UUID = test_lib:answer(Agent, Ch),
 	test_lib:ensureTalking(UUID, InCall),
-	call:hangup(UUID).
+	test_lib:hangup(UUID).
 
 main() ->
-	Queue = admin:new_queue(#{
-		recipe => []
-	}),
+	Queue = admin:new_queue(#{ wrapup_enabled => false, recipe => [] }),
 
-	Agent1 = test_lib:available(admin:new_agent()),
-	timer:sleep(1000),
-	Agent2 = test_lib:available(admin:new_agent()),
-	timer:sleep(1000),
-	Agent3 = test_lib:available(admin:new_agent()),
+	% XXX: ensure all a vqueue is created 
+	{ok, InCall} = call_sup:originate(Queue),
+	test_lib:hangup(InCall),
 
-	receive_call(Queue, Agent1),
-	receive_call(Queue, Agent2),
-	receive_call(Queue, Agent3).
+	Agent1 = test_lib:available(admin:new_agent(#{ ring_timeout => 6 })),
+	timer:sleep(200),
+	Agent2 = test_lib:available(admin:new_agent(#{ ring_timeout => 6 })),
+	timer:sleep(200),
+	Agent3 = test_lib:available(admin:new_agent(#{ ring_timeout => 6 })),
+
+	receive_call(Queue, Agent1, <<"ch1">>),
+	receive_call(Queue, Agent2, <<"ch1">>),
+	receive_call(Queue, Agent3, <<"ch1">>),
+	receive_call(Queue, Agent1, <<"ch2">>),
+	receive_call(Queue, Agent2, <<"ch2">>),
+	receive_call(Queue, Agent3, <<"ch2">>).
