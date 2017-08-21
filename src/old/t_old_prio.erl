33627c4331e6ccd48f8168f6d8e50324521a15b2
Roman Galeev <roman.galeev@ezuce.com>
2017-08-21 12:50:03 +0200

    Tests also call sequence in t_old_prio

--- src/old/t_old_prio.erl
+++ src/old/t_old_prio.erl
@@ -2,10 +2,10 @@
 -export([main/0]).
 -import(ts_core, [wait/1]).
 
-answer(A) ->
+answer(A, In) ->
 	[UUID] = agent:wait_for_call(A),
 	ok = call:answer(UUID),
-	timer:sleep(1000),
+	test_lib:ensureTalking(UUID, In),
 	call:hangup(UUID),
 	call:wait_hangup(UUID),
 	timer:sleep(1000).
@@ -16,5 +16,5 @@ main() ->
 	{ok, In2} = call_sup:originate(<<"QueuePrio">>, [{origination_caller_id_number, <<"203">>}, {origination_caller_id_name, <<"203">>}]),
 	timer:sleep(2000),
 	test_lib:available(A),
-	answer(A),
-	answer(A).
+	answer(A, In2),
+	answer(A, In1).
