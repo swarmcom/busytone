e9fcc3b73057303970546ee6b1b59abf517b8c31
Roman Galeev <roman.galeev@ezuce.com>
2017-08-23 01:29:47 +0200

    Add more conference tests

--- src/core/t_core_agent_conference_to_queue.erl
+++ src/core/t_core_agent_conference_to_queue.erl
@@ -1,15 +1,16 @@
--module(t_core_agent_conference_to_agent).
+-module(t_core_agent_conference_to_queue).
 -export([main/0]).
 -import(ts_core, [wait/1]).
 
 main() ->
-	lager:notice("agent can setup a conference call with another agent"),
+	lager:notice("agent can setup a conference call with a queue"),
 	A = test_lib:available(),
 	{LegA, LegB} = ts_core:setup_talk(A),
 	agent:wait_ev(A, LegB, <<"CHANNEL_BRIDGE">>),
 
+	agent:rpc_call(A, conference_to_queue, [skip, <<"default_queue">>, []]),
+
 	B = test_lib:available(),
-	agent:rpc_call(A, conference_to_agent, [skip, B]),
 	[LegC] = agent:wait_for_call(B),
 	ok = call:answer(LegC),
 
