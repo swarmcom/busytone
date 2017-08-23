35dc8ba74e96fbb26db697c23264e9bf84fdf7e8
Roman Galeev <roman.galeev@ezuce.com>
2017-08-23 14:31:14 +0200

    Add hold/unhold test

---
+++ src/core/t_core_agent_hold.erl
@@ -0,0 +1,22 @@
+-module(t_core_agent_hold).
+-export([main/0]).
+-import(ts_core, [wait/1]).
+
+main() ->
+	lager:notice("agent can hold a call and then bridge back"),
+	A = test_lib:available(),
+	{LegA, LegB} = ts_core:setup_talk(A),
+	agent:wait_ev(A, LegB, <<"CHANNEL_BRIDGE">>),
+	agent:rpc_call(A, hold_channel, [skip]),
+
+	agent:wait_ev(A, LegB, <<"CHANNEL_UNBRIDGE">>),
+	agent:wait_ev(A, LegA, <<"CHANNEL_PARK">>),
+	agent:wait_ev(A, LegB, <<"CHANNEL_PARK">>),
+
+	agent:rpc_call(A, unhold_channel, [skip]),
+	agent:wait_ev(A, LegB, <<"CHANNEL_BRIDGE">>),
+
+	test_lib:ensureTalking(LegA, LegB),
+	call:hangup(LegA),
+	call:hangup(LegB),
+	wait(fun() -> [] = admin:call(inqueues, [all]) end).
