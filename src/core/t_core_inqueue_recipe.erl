1d392f4323fd25159ac98b6e3f19a520e1892292
Roman Galeev <roman.galeev@ezuce.com>
2017-08-23 21:17:29 +0200

    Add announce recipe test

---
+++ src/core/t_core_inqueue_recipe.erl
@@ -0,0 +1,21 @@
+-module(t_core_inqueue_recipe).
+-export([main/0]).
+-import(ts_core, [wait/1]).
+
+main() ->
+	lager:notice("check a recipe "),
+	Queue = admin:new_queue(#{
+		skills => #{ german => true },
+		recipe => [ #{
+			conditions => [ [ticks, '=', 1] ],
+			operations => [ [announce, <<"tone_stream://%(500,0,2600)">>] ],
+			comment => <<"test">>
+		}]
+	}),
+	UUID = call_sup:originate(Queue),
+	admin:call(subscribe, [uuid, UUID]),
+	wait(fun() -> [#{ <<"uuid">> := UUID, <<"state">> := <<"inqueue">>, <<"record">> := <<"inqueue_call">> }]  = admin:call(inqueues, []) end),
+
+	ok = call:detect_tone(UUID, "2600"),
+	call:wait_event(UUID, <<"DETECTED_TONE">>),
+	ok = call:hangup(UUID).
